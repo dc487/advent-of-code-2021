860bcbd909f8aa6c62fb2549559ee15b877e13b0
Dave C <dc487@github.com>
2021-12-30 18:37:34 +0000

    Use priority queue to speed up Djikstra's algorithm

--- day_15_chiton/day_15_chiton.py
+++ day_15_chiton/day_15_chiton.py
@@ -1,7 +1,79 @@
 import pathlib
+from queue import PriorityQueue
 
 def load_input():
     return pathlib.Path("./input.txt").read_text().strip("\n").splitlines()
+
+def run_djikstra_pathfinding(risk_matrix, starting_node, destination_node):
+    width = len(risk_matrix[0])
+    height = len(risk_matrix)
+
+    x = starting_node[0]
+    y = starting_node[1]
+
+    node_queue = PriorityQueue()
+    visited_nodes = set()
+    visited_nodes.add(starting_node)
+    
+    if x - 1 >= 0:
+        node = (x - 1, y)
+        risk_score = risk_matrix[node[0]][node[1]]
+        node_queue.put((risk_score, node))
+
+    if y - 1 >= 0:
+        node = (x, y - 1)
+        risk_score = risk_matrix[node[0]][node[1]]
+        node_queue.put((risk_score, node))
+
+    if x + 1 < width:
+        node = (x + 1, y)
+        risk_score = risk_matrix[node[0]][node[1]]
+        node_queue.put((risk_score, node))
+
+    if y + 1 < height:
+        node = (x, y + 1)
+        risk_score = risk_matrix[node[0]][node[1]]
+        node_queue.put((risk_score, node))
+
+    while not node_queue.empty():
+        (current_risk_score, next_node) = node_queue.get()
+        if next_node in visited_nodes:
+            continue
+
+        visited_nodes.add(next_node)
+
+        if next_node[0] == destination_node[0] and next_node[1] == destination_node[1]:
+            return current_risk_score
+
+        x = next_node[0]
+        y = next_node[1]
+
+        if x - 1 >= 0:
+            node = (x - 1, y)
+            if node not in visited_nodes:
+                risk_score = current_risk_score + risk_matrix[node[0]][node[1]]
+                node_queue.put((risk_score, node))
+
+        if y - 1 >= 0:
+            node = (x, y - 1)
+            if node not in visited_nodes:
+                risk_score = current_risk_score + risk_matrix[node[0]][node[1]]
+                node_queue.put((risk_score, node))
+
+        if x + 1 < width:
+            node = (x + 1, y)
+            if node not in visited_nodes:
+                risk_score = current_risk_score + risk_matrix[node[0]][node[1]]
+                node_queue.put((risk_score, node))
+
+        if y + 1 < height:
+            node = (x, y + 1)
+            if node not in visited_nodes:
+                risk_score = current_risk_score + risk_matrix[node[0]][node[1]]
+                node_queue.put((risk_score, node))
+
+    return 0
+
 
 if __name__ == "__main__":
     input = load_input()
@@ -13,67 +85,28 @@ if __name__ == "__main__":
             risk_line.append(int(character))
         risk_matrix.append(risk_line)
 
-    max_distance = sum([sum(x) for x in risk_matrix])
+    destination_node = (len(risk_matrix[0]) - 1, len(risk_matrix) - 1)
+    print(run_djikstra_pathfinding(risk_matrix, (0,0), destination_node))
 
-    unvisited_nodes = {}
-    for x in range(len(risk_matrix)):
-        for y in range(len(risk_matrix[x])):
-            unvisited_nodes[str(x) + "," + str(y)] = max_distance
+    for i in range(len(input)):
+        line = risk_matrix[i]
+        for j in range(1, 5):
+            for character in input[i]:
+                next_character = (int(character) + j) % 10
+                if next_character < int(character):
+                    next_character += 1
+                line.append(next_character)
 
-    visited_nodes = {}
-    current_node = (0,0)
-    unvisited_nodes["0,0"] = 0
+    for i in range(1, 5):
+        for j in range(len(input)):
+            new_line = risk_matrix[j][:]
+            for k in range(len(risk_matrix[j])):
+                next_character = (risk_matrix[j][k] + i) % 10
+                if next_character < risk_matrix[j][k]:
+                    next_character += 1
+                new_line[k] = next_character
 
-    max_x = 0
-    max_y = 0
+            risk_matrix.append(new_line)
 
-    destination_node = str(len(risk_matrix[0]) - 1) + "," + str(len(risk_matrix) - 1)
-    while destination_node not in visited_nodes.keys():
-        x = current_node[0]
-        y = current_node[1]
-        c_node = str(x) + "," + str(y)
-
-        if (x > max_x):
-            max_x = x
-
-        if (y > max_y):
-            max_y = y
-
-        if c_node in visited_nodes.keys():
-            print("help")
-            break
-
-        if x - 1 >= 0:
-            node = str(x - 1) + "," + str(y)
-            if node not in visited_nodes.keys():
-                risk_score = unvisited_nodes[c_node] + risk_matrix[x - 1][y]
-                unvisited_nodes[node] = min(risk_score, unvisited_nodes[node])
-
-        if y - 1 >= 0:
-            node = str(x) + "," + str(y - 1)
-            if node not in visited_nodes.keys():
-                risk_score = unvisited_nodes[c_node] + risk_matrix[x][y - 1]
-                unvisited_nodes[node] = min(risk_score, unvisited_nodes[node])
-
-        if x + 1 < len(risk_matrix[0]):
-            node = str(x + 1) + "," + str(y)
-            if node not in visited_nodes.keys():
-                risk_score = unvisited_nodes[c_node] + risk_matrix[x + 1][y]
-                unvisited_nodes[node] = min(risk_score, unvisited_nodes[node])
-
-        if y + 1 < len(risk_matrix):
-            node = str(x) + "," + str(y + 1)
-            if node not in visited_nodes.keys():
-                risk_score = unvisited_nodes[c_node] + risk_matrix[x][y + 1]
-                unvisited_nodes[node] = min(risk_score, unvisited_nodes[node])
-
-        visited_nodes[c_node] = unvisited_nodes[c_node]
-        unvisited_nodes.pop(c_node, None)
-        min_score = max_distance
-        for node in unvisited_nodes.keys():
-            if unvisited_nodes[node] < min_score:
-                min_score = unvisited_nodes[node]
-                parts = node.split(",")
-                current_node = (int(parts[0]), int(parts[1]))
-
-    print(visited_nodes[destination_node])
+    destination_node = (len(risk_matrix[0]) - 1, len(risk_matrix) - 1)
+    print(run_djikstra_pathfinding(risk_matrix, (0,0), destination_node))
